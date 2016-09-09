f3bd26291df48bbb5315b0a155cbe6b844239265
Marijn Ophorst <marijn@sensimedia.nl>
2016-09-09 10:37:56 +0200

    fix and php7 only

--- src/Builder.php
+++ src/Builder.php
@@ -3,13 +3,15 @@
 namespace Quibble\Query;
 
 use PDO;
+use PDOStatement;
 
 class Builder
 {
     private $adapter;
     private $tables = [];
-    private $fields = [];
-    private $where = ['*'];
+    private $fields = ['*'];
+    private $wheres = [];
+    private $order = [];
     private $bindables = [];
 
     public function __construct(PDO $adapter, $table, array $init = [])
@@ -24,60 +26,78 @@ class Builder
         }
     }
 
-    public function where($sql, ...$bindables)
+    public function where($sql, ...$bindables) : Builder
     {
         return new static(
             $this->adapter,
             $this->tables,
             [
-                'where' => array_merge($this->where, [$sql]),
+                'wheres' => array_merge($this->wheres, [$sql]),
                 'bindables' => array_merge($this->bindables, $bindables),
             ]
         );
     }
 
-    public function orWhere($sql, ...$bindables)
+    public function andWhere($sql, ...$bindables) : Builder
     {
-        return $this->where("OR $sql", ...$bindables);
+        if (!$this->wheres) {
+            $this->wheres[] = '(1=1)';
+        }
+        return $this->where("AND ($sql)", ...$bindables);
     }
 
-    public function join($table, $style = '', ...$bindables)
+    public function orWhere($sql, ...$bindables) : Builder
+    {
+        if ($this->wheres) {
+            $sql = "OR ($sql)";
+        }
+        return $this->where($sql, ...$bindables);
+    }
+
+    public function join($table, $style = '', ...$bindables) : Builder
     {
         return new static(
             $this->adapter,
             array_merge($this->tables, [sprintf('% JOIN %s', $style, $table)]),
             [
-                'where' => $this->where,
+                'wheres' => $this->wheres,
                 'bindables' => array_merge($this->bindables, $bindables),
             ]
         );
     }
 
-    public function getBindings()
+    public function getBindings() : array
     {
         return $this->bindables;
     }
 
-    public function getSql()
+    public function getSql() : string
     {
         return $this->__toString();
     }
 
-    public function getStatement($driver_params = null)
+    public function getStatement($driver_params = null) : PDOStatement
     {
         return $this->adapter->prepare($this->__toString(), $driver_params);
     }
 
-    public function __toString()
+    public function __toString() : string
     {
         return sprintf(
-            'SELECT %s FROM %s WHERE %s %s %s',
+            'SELECT %s FROM %s %s %s %s',
             implode(', ', $this->fields),
             implode(' ', $this->tables),
-            implode(' ', $this->where),
-            implode(', ', $this->order),
+            $this->wheres ? ' WHERE '.implode(' ', $this->wheres) : '',
+            $this->order ? ' ORDER BY '.implode(', ', $this->order) : '',
             ''
         );
+    }
+
+    public function __call($fn, array $params = [])
+    {
+        $stmt = $this->getStatement();
+        $stmt->execute($this->getBindings());
+        return call_user_func_array([$stmt, $fn], $params);
     }
 }
 
