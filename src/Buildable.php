e8244301c8036bc0a59d769af521831ec405929a
Marijn Ophorst <marijn@sensimedia.nl>
2016-09-11 12:29:28 +0200

    non-static

--- src/Buildable.php
+++ src/Buildable.php
@@ -4,24 +4,22 @@ namespace Quibble\Query;
 
 trait Buildable
 {
-    private static $errMode;
-
-    public static function selectFrom($table) : Select
+    public function selectFrom($table) : Select
     {
         return new Select($this, $table);
     }
 
-    public static function insertInto($table) : Insert
+    public function insertInto($table) : Insert
     {
         return new Insert($this, $table);
     }
 
-    public static function updateTable($table) : Update
+    public function updateTable($table) : Update
     {
         return new Update($this, $table);
     }
 
-    public static function deleteFrom($table) : Delete
+    public function deleteFrom($table) : Delete
     {
         return new Delete($this, $table);
     }
