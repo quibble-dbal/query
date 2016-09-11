bc02df83800d7f4532f5d9d29f0e4764b52afb4e
Marijn Ophorst <marijn@sensimedia.nl>
2016-09-11 12:29:44 +0200

    add tests for modification statements

---
+++ tests/DeleteTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Quibble\Tests;
+
+use Quibble\Sqlite\Adapter;
+use Quibble\Query\DeleteException;
+use Quibble\Query\Buildable;
+use PDO;
+
+/**
+ * Deletion
+ */
+class DeleteTest
+{
+    public function __wakeup()
+    {
+        $this->pdo = new class(':memory') extends Adapter {
+            use Buildable;
+        };
+        $this->pdo->exec(<<<EOT
+DROP TABLE IF EXISTS test;
+CREATE TABLE test (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    foo VARCHAR(255) NOT NULL
+);
+INSERT INTO test (foo) VALUES ('bar'), ('baz'), ('buzz');
+
+EOT
+        );
+    }
+
+    /**
+     * delete should delete a row {?}
+     */
+    public function testDelete()
+    {
+        $res = $this->pdo->deleteFrom('test')
+            ->where(['id' => 1])
+            ->execute();
+        yield assert($res === true);
+    }
+    
+    /**
+     * delete should return false if nothing was deleted {?}
+     */
+    public function testNoDelete(Adapter &$db = null)
+    {
+        $res = $this->pdo->deleteFrom('test')
+            ->where(['id' => 12345])
+            ->execute();
+        yield assert($res === false);
+    }
+    
+    /**
+     * delete should throw an exception if nothing was deleted {?}
+     */
+    public function testNoDeleteWithException(Adapter &$db = null)
+    {
+        $e = null;
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try {
+            $this->pdo->deleteFrom('test')
+                ->where(['id' => 12345])
+                ->execute();
+        } catch (DeleteException $e) {
+        }
+        yield assert($e instanceof DeleteException);
+    }
+}
+
