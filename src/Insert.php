0d2f6c6133582fc76944762f6572de997c5bf00a
Marijn Ophorst <marijn@sensimedia.nl>
2016-10-01 12:31:16 +0200

    cleanup and docs

--- src/Insert.php
+++ src/Insert.php
@@ -8,66 +8,59 @@ use Quibble\Dabble\SqlException;
 
 class Insert extends Builder
 {
-    public function __construct(PDO $adapter, $table)
-    {
-        if (is_array($table)) {
-            $table = array_shift($table);
-        }
-        parent::__construct($adapter, $table);
-    }
-
+    /**
+     * Execute the insert query on one or more supplied sets. If you need to
+     * specify $driver_options, you may do so with the final argument.
+     *
+     * @param array ...$sets A hash of data with key/value pairs.
+     * @return bool True on success.
+     * @throws Quibble\Query\InsertException if any of the sets couldn't be
+     *  inserted and PDO::ERRMODE_EXCEPTION is set.
+     * @throws Quibble\Dabble\SqlException if the query contains an error and
+     *  PDO::ERRMODE_EXCEPTION is set.
+     */
     public function execute(array ...$sets) : bool
     {
         $error = false;
         $errmode = $this->adapter->getAttribute(PDO::ATTR_ERRMODE);
         $result = false;
         $res = 0;
-        $this->bindables['values'] = $sets[0];
-        try {
-            $stmt = $this->getStatement();
-            if (!$stmt) {
-                return false;
+        $driver_options = [];
+        if (count($sets) > 1) {
+            $keys = array_keys($set[0]);
+            $lastArg = end($set);
+            if (array_keys($lastArg) != $keys) {
+                $driver_options = $lastArg;
+                array_pop($sets);
             }
-            foreach ($sets as $set) {
-                $result = $stmt->execute(array_values($set));
-                if ($affectedRows = $stmt->rowCount() and $affectedRows) {
-                    $res += $affectedRows;
+        }
+        foreach ($sets as $set) {
+            $this->bindables['values'] = $set;
+            if ($stmt = $this->getExecutedStatement($driver_options)
+                and $affectedRows = $stmt->rowCount()
+                and $affectedRows
+            ) {
+                $res += $affectedRows;
+                if ($stmt) {
                     continue;
                 }
-                if ($errmode == PDO::ERRMODE_EXCEPTION) {
-                    $info = $stmt->errorInfo();
-                    $msg = "{$info[0]} / {$info[1]}: {$info[2]} - $this ("
-                        .implode(', ', $set).")";
-                    throw new InsertException($msg);
-                }
             }
-            if (!$res) {
-                return false;
-            }
-        } catch (PDOException $e) {
-            if (!isset($set)) {
-                $error = new SqlException("$this", SqlException::PREPARATION, $e);
-            } else {
-                $error = new InsertException(
-                    "$this (".implode(', ', $set).")",
-                    null,
-                    $e
-                );
-            }
-        }
-        if (!$res && !$error) {
-            $error = new InsertException("$this (".implode(', ', $set).")");
-        }
-        if ($error) {
             if ($errmode == PDO::ERRMODE_EXCEPTION) {
-                throw $error;
-            } else {
-                return false;
+                $info = $stmt->errorInfo();
+                $msg = "{$info[0]} / {$info[1]}: {$info[2]} - $this ("
+                    .implode(', ', $set).")";
+                throw new InsertException($msg);
             }
+        }
+        if (!$res) {
+            return false;
         }
         return true;
     }
 
+    /**
+     * @return string
+     */
     public function __toString() : string
     {
         $bindings = $this->bindables['values'];
