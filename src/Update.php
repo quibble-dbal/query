5853d2ee06c376e878e84b8d9d2787d4c13f1355
Marijn Ophorst <marijn@sensimedia.nl>
2016-10-01 12:31:46 +0200

    cleanup and document

--- src/Update.php
+++ src/Update.php
@@ -4,62 +4,51 @@ namespace Quibble\Query;
 
 use PDO;
 use PDOException;
+use Quibble\Dabble\SqlException;
 
 class Update extends Builder
 {
     use Where;
 
-    public function __construct(PDO $adapter, $table)
-    {
-        if (is_array($table)) {
-            $table = array_shift($table);
-        }
-        parent::__construct($adapter, $table);
-    }
-
-    public function execute(array $set) : bool
+    /**
+     * Execute the update statement. The first argument is a hash of key/value
+     * pairs to update. Optional second argument are driver-specific options.
+     *
+     * @param array $set
+     * @param array $driver_options
+     * @return bool True on success.
+     * @throws Quibble\Query\UpdateException if nothing was updated and
+     *  PDO::ERRMODE_EXCEPTION is set.
+     * @throws Quibble\Dabble\SqlException if the SQL contains an error and
+     *  PDO::ERRMODE_EXCEPTION is set.
+     */
+    public function execute(array $set, array $driver_options = []) : bool
     {
         $error = false;
         $errmode = $this->adapter->getAttribute(PDO::ATTR_ERRMODE);
         $this->bindables['values'] = $set;
         $result = false;
-        try {
-            $stmt = $this->getStatement();
-            if (!$stmt) {
-                return false;
-            }
-            $result = $stmt->execute($this->getBindings());
-            if ($affectedRows = $stmt->rowCount() and $affectedRows) {
-                return true;
-            }
-            if ($errmode == PDO::ERRMODE_EXCEPTION) {
-                $info = $stmt->errorInfo();
-                $msg = "{$info[0]} / {$info[1]}: {$info[2]} - $this ("
-                    .implode(', ', $this->getBindings()).")";
-                throw new UpdateException($msg);
-            } else {
-                return false;
-            }
-        } catch (PDOException $e) {
-            $error = new UpdateException(
-                "$this (".implode(', ', $set).")",
-                null,
-                $e
-            );
+        $stmt = $this->getExecutedStatement($driver_options);
+        if (!$stmt) {
+            return false;
         }
-        if (!$result && !$error) {
-            $error = new UpdateException("$this (".implode(', ', $set).")");
+        if ($affectedRows = $stmt->rowCount() and $affectedRows) {
+            return true;
         }
-        if ($error) {
-            if ($errmode == PDO::ERRMODE_EXCEPTION) {
-                throw $error;
-            } else {
-                return false;
-            }
+        if ($errmode == PDO::ERRMODE_EXCEPTION) {
+            $info = $stmt->errorInfo();
+            $msg = "{$info[0]} / {$info[1]}: {$info[2]} - $this ("
+                .implode(', ', $this->getBindings()).")";
+            throw new UpdateException($msg);
+        } else {
+            return false;
         }
         return true;
     }
 
+    /**
+     * @return string
+     */
     public function __toString() : string
     {
         $modifiers = [];
