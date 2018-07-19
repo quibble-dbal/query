de1fb1f93f0f42a4dadb92c3ea469a4d00defb6c
Marijn Ophorst <marijn@sensimedia.nl>
2018-07-19 11:11:24 +0200

    to toast format

--- tests/insert.php
+++ tests/insert.php
@@ -1,24 +1,19 @@
 <?php
 
-namespace Quibble\Tests;
-
 use Quibble\Sqlite\Adapter;
 use Quibble\Dabble\SqlException;
 use Quibble\Query\InsertException;
 use Quibble\Query\Buildable;
-use PDO;
 
 /**
  * Insertions
  */
-class InsertTest
-{
-    public function __wakeup()
-    {
-        $this->pdo = new class(':memory:') extends Adapter {
+return function ($test) : Generator {
+    $test->beforeEach(function () use (&$pdo) {
+        $pdo = new class(':memory:') extends Adapter {
             use Buildable;
         };
-        $this->pdo->exec(<<<EOT
+        $pdo->exec(<<<EOT
 DROP TABLE IF EXISTS test;
 CREATE TABLE test (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,41 +22,32 @@ CREATE TABLE test (
 
 EOT
         );
-    }
+    });
 
-    /**
-     * insert should insert a new row {?}
-     */
-    public function testInsert()
-    {
-        $res = $this->pdo->insertInto('test')
+    /** insert should insert a new row */
+    yield function () use ($pdo) {
+        $res = $pdo->insertInto('test')
             ->execute(['foo' => 'monomelodies']);
         yield assert($res === true);
-    }
+    };
 
-    /**
-     * insert should return false if nothing was inserted {?}
-     */
-    public function testNoInsert()
-    {
-        $res = $this->pdo->insertInto('test2')
+    /** insert should return false if nothing was inserted */
+    yield function () use ($pdo) {
+        $res = $pdo->insertInto('test2')
             ->execute(['foo' => null]);
         yield assert($res === false);
-    }
+    };
 
-    /**
-     * insert should throw an exception if nothing was inserted {?}
-     */
-    public function testNoInsertWithException()
-    {
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    /** insert should throw an exception if nothing was inserted */
+    yield function () use ($pdo) {
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $e = null;
         try {
-            $this->pdo->insertInto('test2')
+            $pdo->insertInto('test2')
                 ->execute(['foo' => null]);
         } catch (SqlException $e) {
         }
         yield assert($e instanceof SqlException);
-    }
-}
+    };
+};
 
