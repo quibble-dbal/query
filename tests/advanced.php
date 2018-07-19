fc9a1ded2becc7d46d5f960dd9e7bbadaf11733e
Marijn Ophorst <marijn@sensimedia.nl>
2018-07-19 11:27:12 +0200

    renamed and move to toast format

--- tests/advanced.php
+++ tests/advanced.php
@@ -1,21 +1,15 @@
 <?php
 
-namespace Quibble\Tests;
-
 use Quibble\Query\Select;
 use Quibble\Sqlite\Adapter;
-use PDO;
-use Generator;
 
 /**
  * Adavanced selecting of data.
  */
-class AdvancedSelectTest
-{
-    public function __wakeup()
-    {
-        $this->pdo = new Adapter(':memory:');
-        $this->pdo->exec(<<<EOT
+return function ($test) : Generator {
+    $test->beforeEach(function () use (&$pdo) {
+        $pdo = new Adapter(':memory:');
+        $pdo->exec(<<<EOT
 DROP TABLE IF EXISTS test;
 CREATE TABLE test (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,88 +26,68 @@ INSERT INTO test2 (bar) VALUES ('bar'), ('baz'), ('buzz');
 
 EOT
         );
-    }
+    });
 
-    /**
-     * We can use a builder to union tables distinctly {?} or with the ALL
-     * SQL keywords.
-     */
-    public function unions()
-    {
-        $query = new Select($this->pdo, 'test');
-        $query->union(new Select($this->pdo, 'test2'));
+    /** We can use a builder to union tables distinctly or with the ALL SQL keyword. */
+    yield function () use (&$pdo) {
+        $query = new Select($pdo, 'test');
+        $query->union(new Select($pdo, 'test2'));
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
         yield assert(count($result) == 3);
 
-        $query = new Select($this->pdo, 'test');
-        $query->unionAll(new Select($this->pdo, 'test2'));
+        $query = new Select($pdo, 'test');
+        $query->unionAll(new Select($pdo, 'test2'));
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
         yield assert(count($result) == 6);
-    }
+    };
 
-    /**
-     * We can also use the builder to join two tables {?}.
-     */
-    public function joins()
-    {
-        $query = new Select($this->pdo, 'test');
+    /** We can also use the builder to join two tables. */
+    yield function () use (&$pdo) {
+        $query = new Select($pdo, 'test');
         $result = $query->join('test2 USING (id)')
             ->fetch(PDO::FETCH_ASSOC);
         yield assert(count($result) == 3);
-    }
+    };
 
-    /**
-     * Fetching using `generator` gives us a generator {?}.
-     */
-    public function generator()
-    {
-        $query = new Select($this->pdo, 'test');
+    /** Fetching using `generator` gives us a generator. */
+    yield function () use (&$pdo) {
+        $query = new Select($pdo, 'test');
         $result = $query->generate(PDO::FETCH_ASSOC);
         yield assert($result instanceOf Generator);
-    }
+    };
 
-    /**
-     * Fetching using `count` gives us the count {?}.
-     */
-    public function counting()
-    {
-        $query = new Select($this->pdo, 'test');
+    /** Fetching using `count` gives us the count. */
+    yield function () use (&$pdo) {
+        $query = new Select($pdo, 'test');
         $result = $query->count();
         yield assert($result == 3);
-    }
+    };
 
-    /**
-     * The `in` method correctly makes an IN statement {?}.
-     */
-    public function in()
-    {
-        $query = new Select($this->pdo, 'test');
+    /** The `in` method correctly makes an IN statement. */
+    yield function () use (&$pdo) {
+        $query = new Select($pdo, 'test');
         $result = $query->where($query->in('id', [1, 2]))->fetchAll();
         yield assert(count($result) == 2);
-    }
+    };
 
-    /**
-     * If a bindable is itself a Select query it becomes a subquery {?}.
-     */
-    public function subQuery()
-    {
-        $query = new Select($this->pdo, 'test');
-        $subquery = new Select($this->pdo, 'test2');
+    /** If a bindable is itself a Select query it becomes a subquery. */
+    yield function () use (&$pdo) {
+        $query = new Select($pdo, 'test');
+        $subquery = new Select($pdo, 'test2');
         $subquery->where('bar = ?', 'bar')
             ->orWhere('bar = ?', 'baz')
             ->select('bar');
         $query->where('foo IN (?)', $subquery);
         $result = $query->fetchAll();
         yield assert(count($result) == 2);
-    }
+    };
 
     /**
      * If the SQL part of a WHERE call is itself callable, it becomes a grouping
-     * function/object {?}.
+     * function/object.
      */
-    public function groupedWhere()
-    {
-        $query = new Select($this->pdo, 'test');
+    yield function () use (&$pdo) {
+        $query = new Select($pdo, 'test');
         $result = $query->where('id > ?', 1)
             ->andWhere(function ($group) {
                 $group->where('foo', 'bar')
@@ -121,6 +95,6 @@ EOT
             })
             ->fetchAll();
         yield assert(count($result) == 1);
-    }
-}
+    };
+};
 
