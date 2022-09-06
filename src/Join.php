2ce121e5c121b5e190382ce344deff262ceae675
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-06 14:33:09 +0200

    v1 of join implementation

--- src/Join.php
+++ src/Join.php
@@ -4,5 +4,40 @@ namespace Quibble\Query;
 
 class Join
 {
+    private string $table;
+
+    private string $joinCondition;
+
+    private string $direction;
+
+    public function table(string $table) : self
+    {
+        $this->table = $table;
+        return $this;
+    }
+
+    public function using(string $field) : self
+    {
+        $this->joinCondition = "USING($field)";
+        return $this;
+    }
+
+    public function on(string $on) : self
+    {
+        $this->joinCondition = "ON $on";
+    }
+
+    public function __toString() : string
+    {
+        if (!isset($this->joinCondition)) {
+            throw new JoinException("joinCondition must be set; call either `using` or `on`.");
+        }
+        return preg_replace("@\s+@", ' ', sprintf(
+            '%s JOIN %s %s',
+            $this->direction ?? '',
+            $this->table,
+            $this->joinCondition
+        ));
+    }
 }
 
