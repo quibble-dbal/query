c3f65b3d305ca55b7e67adfc72cae9a8d7ad0f9a
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-06 15:09:53 +0200

    allow Select objects as tables

--- src/Join.php
+++ src/Join.php
@@ -8,11 +8,38 @@ class Join
 
     private string $joinCondition;
 
-    private string $direction;
+    private string $type;
 
-    public function table(string $table) : self
+    private array $bindings = [];
+
+    public function inner(string|Select $table) : self
     {
-        $this->table = $table;
+        if (isset($this->type)) {
+            throw new JoinException("The join style (inner, left, right, full) can only be set once.");
+        }
+        $this->type = 'INNER';
+        $this->table($table);
+        return $this;
+    }
+
+    public function left(string|Select $table) : self
+    {
+        $this->type = 'LEFT';
+        $this->table($table);
+        return $this;
+    }
+
+    public function right(string|Select $table) : self
+    {
+        $this->type = 'RIGHT';
+        $this->table($table);
+        return $this;
+    }
+
+    public function full(string|Select $table) : self
+    {
+        $this->type = 'FULL';
+        $this->table($table);
         return $this;
     }
 
@@ -22,10 +49,18 @@ class Join
         return $this;
     }
 
-    public function on(string $on) : self
+    public function on(string $on, ...$bindables) : self
     {
         $this->joinCondition = "ON $on";
+        if ($bindables) {
+            $this->bindings = array_merge($this->bindings, $bindables);
+        }
         return $this;
+    }
+
+    public function getBindings() :? array
+    {
+        return $this->bindings ?? null;
     }
 
     public function __toString() : string
@@ -35,10 +70,20 @@ class Join
         }
         return preg_replace("@\s+@", ' ', sprintf(
             '%s JOIN %s %s',
-            $this->direction ?? '',
+            $this->type,
             $this->table,
             $this->joinCondition
         ));
+    }
+
+    private function table(string|Select $table) : void
+    {
+        if (is_object($table)) {
+            $bindings = $table->getBindings();
+            $this->bindings = array_merge($bindings, $this->bindings);
+            $table = "$table";
+        }
+        $this->table = $table;
     }
 }
 
