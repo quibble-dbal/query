d46f2dda8338f20e46e34bbe41ab8a44089208dc
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-06 15:58:47 +0200

    place the in/notIn methods on the _group_, that makes way more sense + nicer code

--- src/Group.php
+++ src/Group.php
@@ -12,7 +12,7 @@ class Group extends Builder
 
     public function __toString() : string
     {
-        return implode(' ', $this->wheres);
+        return array_reduce($this->wheres, [$this, 'recursiveImplode'], '');
     }
 
     public function getStatement(array $driver_options = []) :? PDOStatement
@@ -23,6 +23,19 @@ class Group extends Builder
     public function getExecutedStatement(array $driver_options = []) :? PDOStatement
     {
         throw new DomainException("Cannot get a statement for a group.");
+    }
+
+    public function in($field, array $values) : self
+    {
+        $sql = "$field IN (";
+        $sql .= implode(', ', array_fill(0, count($values), '?'));
+        $sql .= ')';
+        return $this->where($sql, ...array_values($values));
+    }
+
+    public function notIn($field, array $values) : self
+    {
+        return $this->in("$field NOT", $values);
     }
 }
 
