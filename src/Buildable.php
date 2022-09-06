59b9a318eb6c576a94777b0741c6581414bb171d
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-06 16:57:28 +0200

    add type hints

--- src/Buildable.php
+++ src/Buildable.php
@@ -4,22 +4,22 @@ namespace Quibble\Query;
 
 trait Buildable
 {
-    public function select($table) : Select
+    public function select(string|Select $table) : Select
     {
         return new Select($this, $table);
     }
 
-    public function insert($table) : Insert
+    public function insert(string $table) : Insert
     {
         return new Insert($this, $table);
     }
 
-    public function update($table) : Update
+    public function update(string $table) : Update
     {
         return new Update($this, $table);
     }
 
-    public function delete($table) : Delete
+    public function delete(string $table) : Delete
     {
         return new Delete($this, $table);
     }
