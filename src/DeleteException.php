71b14226368305f3a6a02b4f127d97b7cd47bdc3
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-07 13:01:14 +0200

    add doccomment and type hints

--- src/DeleteException.php
+++ src/DeleteException.php
@@ -7,10 +7,20 @@
 namespace Quibble\Query;
 
 use Quibble\Dabble\Exception;
+use Throwable;
 
+/**
+ * Exception thrown if a delete query did not actually remove anything (thrown
+ * when ERRMODE is set to EXCEPTION).
+ */
 class DeleteException extends Exception
 {
-    public function __construct($message = '', $code = null, $previous = null)
+    /**
+     * @param string $message
+     * @param int $code
+     * @param Throwable $previous
+     */
+    public function __construct(string $message = '', int $code = null, Throwable $previous = null)
     {
         parent::__construct($message, self::NOAFFECTEDROWS, $previous);
     }
