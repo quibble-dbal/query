1fb83e8a21702933cd99a7e6fdd3b51a5185bd4a
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-07 13:49:02 +0200

    doccomments

--- src/Where.php
+++ src/Where.php
@@ -2,10 +2,21 @@
 
 namespace Quibble\Query;
 
+/**
+ * A trait supplying the WHERE methods.
+ */
 trait Where
 {
-    protected $wheres = [];
+    protected array $wheres = [];
 
+    /**
+     * Add a normal AND WHERE condition. If the $sql is supplied as a callable,
+     * it is called with a $group parameter.
+     *
+     * @param string|callable $sql
+     * @param mixed ...$bindables
+     * @return self
+     */
     public function where(string|callable $sql, mixed ...$bindables) : self
     {
         $sql = $this->checkGroup($sql);
@@ -16,6 +27,13 @@ trait Where
         return $this;
     }
     
+    /**
+     * See `where`, only with an "OR" instead of "AND".
+     *
+     * @param string|callable $sql
+     * @param mixed ...$bindables
+     * @return self
+     */
     public function orWhere(string|callable $sql, mixed ...$bindables) : self
     {
         $sql = $this->checkGroup($sql);
@@ -26,6 +44,12 @@ trait Where
         return $this;
     }
 
+    /**
+     * Internal helper to check (and run) for a grouping.
+     *
+     * @param string|callable $sql
+     * @return string
+     */
     protected function checkGroup(string|callable $sql) : string
     {
         if (is_callable($sql)) {
@@ -40,6 +64,13 @@ trait Where
         return $sql;
     }
 
+    /**
+     * Recursively "implode" the where array to a single string.
+     *
+     * @param string $carry The SQL so far.
+     * @param string|array $item
+     * @return string
+     */
     protected function recursiveImplode(string $carry, string|array $item) : string
     {
         static $condition = 'AND';
