092ce8af54633d717ef58fd50aeba3e669211e69
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-07 13:06:43 +0200

    add doccomments and type hints

--- src/Group.php
+++ src/Group.php
@@ -5,27 +5,58 @@ namespace Quibble\Query;
 use DomainException;
 use PDOStatement;
 
+/**
+ * A "group" statement. In `quibble/query`, the concept of "groups" is basically
+ * an isolated sub-statement, most commonly use when supplying `where` or
+ * `orWhere` with a callback.
+ */
 class Group extends Builder
 {
     use Where;
     use Bindable;
 
+    /**
+     * @return string
+     */
     public function __toString() : string
     {
         return array_reduce($this->wheres, [$this, 'recursiveImplode'], '');
     }
 
+    /**
+     * Added because this is illegal; a group is an incomplete statement by
+     * definition.
+     *
+     * @param array $driver_options
+     * @return PDOStatement|null
+     * @throws DomainException This is always thrown.
+     */
     public function getStatement(array $driver_options = []) :? PDOStatement
     {
         throw new DomainException("Cannot get a statement for a group.");
     }
 
+    /**
+     * Added because this is illegal; a group is an incomplete statement by
+     * definition.
+     *
+     * @param array $driver_options
+     * @return PDOStatement|null
+     * @throws DomainException This is always thrown.
+     */
     public function getExecutedStatement(array $driver_options = []) :? PDOStatement
     {
         throw new DomainException("Cannot get a statement for a group.");
     }
 
-    public function in($field, array $values) : self
+    /**
+     * Generate an `IN (...)` clause based on an array of known values.
+     *
+     * @param string $field The field to check on.
+     * @param array $values The values to check for.
+     * @return self
+     */
+    public function in(string $field, array $values) : self
     {
         $sql = "$field IN (";
         $sql .= implode(', ', array_fill(0, count($values), '?'));
@@ -33,7 +64,14 @@ class Group extends Builder
         return $this->where($sql, ...array_values($values));
     }
 
-    public function notIn($field, array $values) : self
+    /**
+     * Generate a `NOT IN (...)` clause based on an array of known values.
+     *
+     * @param string $field The field to check on.
+     * @param array $values The values to check for.
+     * @return self
+     */
+    public function notIn(string $field, array $values) : self
     {
         return $this->in("$field NOT", $values);
     }
