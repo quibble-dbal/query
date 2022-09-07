e12a5a4d038f05340eb7c2afee5d523bf08bb2a9
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-07 13:35:23 +0200

    doccomments and type hints

--- src/UpdateException.php
+++ src/UpdateException.php
@@ -7,10 +7,19 @@
 namespace Quibble\Query;
 
 use Quibble\Dabble\Exception;
+use Throwable;
 
+/**
+ * Exception thrown when an UPDATE query did not actually change anything.
+ */
 class UpdateException extends Exception
 {
-    public function __construct($message = '', $code = null, $previous = null)
+    /**
+     * @param string $message
+     * @param int $code
+     * @param Throwable $previous
+     */
+    public function __construct(string $message = '', int $code = null, Throwable $previous = null)
     {
         parent::__construct($message, self::NOAFFECTEDROWS, $previous);
     }
